lazyfrosch/puppet-lint

commit b91d0058da6265ade231bfcd5516bb0050ecc3d5

Fix the check_classes check for certain cases.
Examples:

File: modules/foobar/manifests/init.pp
Content: 'class bar { }'

File: modules/foobar/manifests/fail.pp
Content: 'class bar::fail {}'

These 2 examples do not throw the autoloader error while they really should.

lib/puppet-lint/plugins/check_classes.rb
@@ -26,9 +26,9 @@ class PuppetLint::Plugins::CheckClasses < PuppetLint::CheckPlugin
         split_title = title_token.value.split('::')
         mod = split_title.first
         if split_title.length > 1
-          expected_path = "#{mod}/manifests/#{split_title[1..-1].join('/')}.pp"
+          expected_path = "/#{mod}/manifests/#{split_title[1..-1].join('/')}.pp"
         else
-          expected_path = "#{title_token.value}/manifests/init.pp"
+          expected_path = "/#{title_token.value}/manifests/init.pp"
         end
 
         unless fullpath.end_with? expected_path

commit 88b1a20ddbb1dd66864c4b689262668e84da98b0

Fix autoload layout check to allow vendor path naming
This fixes a problem introduced by b91d0058da6265ade231bfcd5516bb0050ecc3d5
if you have a module checked out in a directory which is actually the module
name you won't have a problem.

But when the directory is named "vendor-module" or "puppet-module" the check
fails.

The named commit added a slash as prefix to avoid detecting wrong module names,
I've added a dash.

lib/puppet-lint/plugins/check_classes.rb
@@ -23,16 +23,17 @@ PuppetLint.new_check(:autoloader_layout) do
         split_title = title_token.value.split('::')
         mod = split_title.first
         if split_title.length > 1
-          expected_path = "/#{mod}/manifests/#{split_title[1..-1].join('/')}.pp"
+          expected_path = "#{mod}/manifests/#{split_title[1..-1].join('/')}.pp"
         else
-          expected_path = "/#{title_token.value}/manifests/init.pp"
+          expected_path = "#{title_token.value}/manifests/init.pp"
         end
 
         if PuppetLint.configuration.relative
           expected_path = expected_path.gsub(/^\//,'').split('/')[1..-1].join('/')
         end
 
-        unless fullpath.end_with? expected_path
+        expected_path_re = Regexp.new("[/-]" + Regexp.escape(expected_path) + "$")
+        unless expected_path_re.match(fullpath)
           notify :error, {
             :message => "#{title_token.value} not in autoload module layout",
             :line    => title_token.line,